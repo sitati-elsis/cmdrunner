92ba1eb4bf878e206a6514cc52670db300eb0ff8
sitati-elsis <sitatielsis@gmail.com>
2021-09-21 19:00:00 +0300

    Use Autofields to autoincrement primary keys

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Machine(models.Model):
-    machine_id = models.IntegerField(primary_key=True)
+    machine_id = models.AutoField(primary_key=True)
     ip_address = models.CharField(max_length=15)
     created_at = models.DateTimeField(auto_now_add=True)
     hostname = models.CharField(max_length=100)
@@ -12,14 +12,14 @@ class Machine(models.Model):
         return f'{self.ip_address}'
 
 class Command(models.Model):
-    command_id = models.IntegerField(primary_key=True)
+    command_id = models.AutoField(primary_key=True)
     command_name = models.CharField(max_length=10)
 
     def __str__(self):
         return f'{self.command_name}'
 
 class CommandOptions(models.Model):
-    option_id = models.IntegerField(primary_key=True)
+    option_id = models.AutoField(primary_key=True)
     option = models.CharField(max_length=15)
     description = models.CharField(max_length=100)
     command = models.ForeignKey(Command, on_delete=models.CASCADE, related_name='commandoption')
