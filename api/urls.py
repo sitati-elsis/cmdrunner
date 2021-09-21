5bd46c8e2152777a612ad0d087623c6606da7800
sitati-elsis <sitatielsis@gmail.com>
2021-09-21 14:24:04 +0300

    Add user login endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views as rest_auth_views
 
 from api import viewsets, views
 
 urlpatterns = [
-    path('command/<int:command_id>/execute/', views.execute, name='execute')
+    path('command/<int:command_id>/execute/', views.execute, name='execute'),
+    path('login/', rest_auth_views.obtain_auth_token, name='login'),
 ]
 
 router = DefaultRouter()
