dd59b8d310adc2d7aaaadf55ae0d7b49780efd7a
sitati-elsis <sitatielsis@gmail.com>
2021-09-22 08:24:23 +0300

    Remove polling for database service - This was meant to ensure that the database service starts up before the cmdrunner service so that migrations can be applied without error. - This made the cmdrunner container was tightly coupled to PostgreSQL. - This can be avoided by explicitly starting dependent services first.

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,12 +1,5 @@
 set -e
 
-until PGPASSWORD=$POSTGRES_PASSWORD pg_isready -h "db" -p "5432" -q; do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
-
->&2 echo "Postgres is up - executing command"
-
 # Apply database migrations
 echo "Apply database migrations"
 python manage.py migrate
